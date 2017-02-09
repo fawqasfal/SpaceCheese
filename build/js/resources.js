8e677783184575d1c424f71e66daf2fca39ddac0
omarhegazy <omarbicboi@gmail.com>
2017-02-09 00:49:08 -0500

    Removed build from gitignore so it's easy to just clone and open index.html

---
+++ build/js/resources.js
@@ -0,0 +1,87 @@
+game.resources = [
+    {
+        "name": "dst-inertexponent",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "cling",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "jump",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "stomp",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "area01_bkg0",
+        "type": "image",
+        "src": "data/img/area01_bkg0.png"
+    },
+    {
+        "name": "area01_bkg1",
+        "type": "image",
+        "src": "data/img/area01_bkg1.png"
+    },
+    {
+        "name": "bg",
+        "type": "image",
+        "src": "data/img/bg.png"
+    },
+    {
+        "name": "32x32_font",
+        "type": "image",
+        "src": "data/img/font/32x32_font.png"
+    },
+    {
+        "name": "title_screen",
+        "type": "image",
+        "src": "data/img/gui/title_screen.png"
+    },
+    {
+        "name": "area01_level_tiles",
+        "type": "image",
+        "src": "data/img/map/area01_level_tiles.png"
+    },
+    {
+        "name": "asteroid",
+        "type": "image",
+        "src": "data/img/sprite/asteroid.png"
+    },
+    {
+        "name": "black_enemy",
+        "type": "image",
+        "src": "data/img/sprite/black_enemy.png"
+    },
+    {
+        "name": "cheese",
+        "type": "image",
+        "src": "data/img/sprite/cheese.png"
+    },
+    {
+        "name": "playerShip2_red",
+        "type": "image",
+        "src": "data/img/sprite/playerShip2_red.png"
+    },
+    {
+        "name": "white_enemy",
+        "type": "image",
+        "src": "data/img/sprite/white_enemy.png"
+    },
+    {
+        "name": "area01",
+        "type": "tmx",
+        "src": "data/map/area01.tmx"
+    },
+    {
+        "name": "area02",
+        "type": "tmx",
+        "src": "data/map/area02.tmx"
+    }
+];
